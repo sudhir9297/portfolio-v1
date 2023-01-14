d2362510cd8ad11debf3bee2cc6e3e2b625a5440
sudhir <sudhir9297@gmail.com>
2023-01-14 10:32:19 +0530

    added custom hooks for scroll direction and used in header

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -2,44 +2,28 @@ import React, { useState, useEffect, useCallback } from 'react';
 import { gsap } from 'gsap';
 import Hamburger from '../Hamburger';
 import ColorModeButton from '../ColorModeButton';
+import { useScrollDirection } from '../../lib/customHooks/useScrollDirection';
 
 function Header() {
-  const [y, setY] = useState(0);
-  const [isUp, setIsUp] = useState(false);
-
-  const handleNavigation = useCallback(
-    (e) => {
-      const window = e.currentTarget;
-
-      if (y >= window.scrollY && !isUp) {
-        gsap.to('#header', {
-          yPercent: 0,
-          delay: 0.3,
-          opacity: 1,
-          ease: 'power.easeInOut',
-        });
-        setIsUp(true);
-      } else if (y < window.scrollY && isUp) {
-        gsap.to('#header', {
-          yPercent: -100,
-          delay: 0.3,
-          opacity: 0,
-          ease: 'power.easeInOut',
-        });
-        setIsUp(false);
-      }
-      setY(window.scrollY);
-    },
-    [y]
-  );
+  const scrollDirection = useScrollDirection();
 
   useEffect(() => {
-    setY(window.scrollY);
-    window.addEventListener('scroll', handleNavigation);
-    return () => {
-      window.removeEventListener('scroll', handleNavigation);
-    };
-  }, [handleNavigation]);
+    if (scrollDirection === 'up') {
+      gsap.to('#header', {
+        yPercent: 0,
+        delay: 0.3,
+        opacity: 1,
+        ease: 'power.easeInOut',
+      });
+    } else if (scrollDirection === 'down') {
+      gsap.to('#header', {
+        yPercent: -100,
+        delay: 0.3,
+        opacity: 0,
+        ease: 'power.easeInOut',
+      });
+    }
+  }, [scrollDirection]);
 
   return (
     <header id="header" className="pt-5 fixed top-0 w-full box-border">
